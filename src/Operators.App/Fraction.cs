710b5a0c496a1883cab4cbf6ab23af37b68748f5
gscaduto <giovani.scaduto@gmail.com>
2023-06-21 19:23:29 -0300

    operators app

---
+++ src/Operators.App/Fraction.cs
@@ -0,0 +1,59 @@
+﻿namespace Operators.App
+{
+    public class Fraction : IEquatable<Fraction>
+    {
+        public int Numerator { get; private set; }
+        public int Denominator { get; private set; }
+
+        public Fraction(int numerator, int denominator)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+
+        public double Result =>
+            (double)Numerator / Denominator;
+
+        public override string ToString() =>
+            $"{Numerator:n2}/{Denominator:n2}";
+
+        public static Fraction operator * (Fraction left, Fraction right) =>
+            new Fraction(left.Numerator * right.Numerator, left.Denominator * right.Denominator);
+
+        public static Fraction operator * (Fraction fraction, int number) =>
+            new Fraction(fraction.Numerator * number, fraction.Denominator);
+
+        public static bool operator < (Fraction left, Fraction right) =>
+            left.Result < right.Result;
+
+        public static bool operator > (Fraction left, Fraction right) =>
+            left.Result < right.Result;
+
+        public static bool operator <= (Fraction left, Fraction right) =>
+          left.Result < right.Result;
+
+        public static bool operator >= (Fraction left, Fraction right) =>
+            left.Result < right.Result;
+
+        public static bool operator == (Fraction left, Fraction right) =>
+            left.Equals(right);
+
+        public static bool operator != (Fraction left, Fraction right) =>
+           !left.Equals(right);
+
+        public bool Equals(Fraction other) =>
+            Result == other.Result;
+        
+        public override bool Equals(object obj)
+        {
+            var f = obj as Fraction;
+
+            if(f == null) return false; 
+
+            return Equals(f);
+        }
+
+        public override int GetHashCode() =>
+            Result.GetHashCode();
+    }
+}
---
+++ src/Operators.App/Program.cs
@@ -0,0 +1,6 @@
+﻿using Operators.App;
+
+Fraction f1 = new(3, 2);
+Fraction f2 = new(6, 4);
+
+Console.WriteLine($"Fraction 1 is equals Fraction 2 ? {f1 == f2}");

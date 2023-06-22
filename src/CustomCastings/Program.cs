f712d294d5f0fe869a82ed24f47670c3f1182b75
gioscaduto <41067850+gioscaduto@users.noreply.github.com>
2023-06-22 12:52:49 -0300

    ok

--- src/CustomCastings/Program.cs
+++ src/CustomCastings/Program.cs
@@ -2,10 +2,10 @@
 
 AlphabetLetter letter = new('g');
 
-Console.WriteLine("Using casting explicit");
+Console.WriteLine("Using explicit casting");
 char c = (char)letter;
 Console.WriteLine(c);
 
-Console.WriteLine("Using casting implicit");
+Console.WriteLine("Using implicit casting");
 AlphabetLetter otherLetter = 'a';
 Console.WriteLine(otherLetter.Character);
